2df364e5a1788c48d2fb51564bce11a0a0c9891d
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-11 18:45:50 +0100

    Grid now printing to page, fixed HTML error

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let container = document.querySelector('.contain');
-container.style.gridTemplateColumns = "repeat(16 , 1fr)";
-container.style.gridTemplateRows = "repeat(16 , 1fr)";
-
+const container = document.querySelector('.board');
 
 
 
 for (let i = 0; i < 256; i++){
-    let cell = document.createElement('div');
+    const cell = document.createElement('div');
     cell.style.backgroundColor = "blue";
-    
+    container.appendChild(cell);
 }
+
+
 
 
