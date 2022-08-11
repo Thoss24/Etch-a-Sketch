566364a7083c80f5568700cfbf762a91f45d6da3
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-11 17:15:25 +0100

    Function made to create 256 div elements, not appended to page yet

---
+++ script.js
@@ -0,0 +1,14 @@
+let container = document.querySelector('.contain');
+container.style.gridTemplateColumns = "repeat(16 , 1fr)";
+container.style.gridTemplateRows = "repeat(16 , 1fr)";
+
+
+
+
+for (let i = 0; i < 256; i++){
+    let cell = document.createElement('div');
+    cell.style.backgroundColor = "blue";
+    
+}
+
+
