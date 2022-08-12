674f8b3fbd96e0157ab4e20a6445451521a88115
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-12 16:56:55 +0100

    buttons with onclick events to change color of squares working

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// global variable for square color value
+let color = "black";
+
 // Function to create 16x16 grid
 let gridSquares = 256;
 
@@ -7,7 +10,7 @@ const container = document.querySelector('.board');
 for (let i = 0; i < squares; i++){
     const cell = document.createElement('div');
     
-    cell.addEventListener('mouseenter', () => cell.style.backgroundColor = "black");
+    cell.addEventListener('mouseenter', colorSquare);
 
     cell.style.backgroundColor = "white";
     cell.setAttribute('id', "cell-style");
@@ -15,12 +18,22 @@ for (let i = 0; i < squares; i++){
 }
 }
 createGrid(gridSquares);
- 
 
+// function to color the squares. (this) refers to the element that received the event. here, (this) is referring to the div that is being hovered over by the user.
+function colorSquare() {
+    this.style.backgroundColor = color;
+}
+ 
+// function to reset Etch-a-Sketch board
 function resetBoard() {
     const container = document.querySelector('.board');
     let cell = container.querySelectorAll('div')
     cell.forEach((div) => div.style.backgroundColor = "white");
 }
 
+// function to change color of the squares. the (select) parameter refers to the color which has been passed into the changeColor function inside the onclick event within the HTML file.
+function changeColor(select) {
+    color = select;                    /* When the changeColor function is invoked with a different color passed in as an argument, 
+                                          the value of the global (color) variable is being changed. changeColor('grey) changes (color) to grey instead of its default value of black.*/
+}
 
