47b43e0a8a9daf4e38f76e0f2b0681a367dda1fb
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-12 15:03:11 +0100

    mouse over/ mouse leave event working

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+// Loop to create 16x16 grid
+function createGrid() {
 const container = document.querySelector('.board');
-
-
 
 for (let i = 0; i < 256; i++){
     const cell = document.createElement('div');
+    
+    cell.addEventListener('mouseenter', () => 
+    console.log('Mouse Enter'));
+
+    cell.addEventListener('mouseleave', () =>
+    console.log('Mouse Leave'));
+
     cell.style.backgroundColor = "blue";
+    cell.setAttribute('id', "cell-style");
     container.appendChild(cell);
 }
 
-
+}
+createGrid();
 
 
